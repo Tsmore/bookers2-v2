3e632e96fb007800c0b8ca8704f4b3ff536689e8
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-21 04:12:13 +0000

    users/booksバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, uniqueness: true, presence: true, length: {maximum: 20}
+  has_one_attached :profile_image
+  has_many :books
+
+
+  validates :name, uniqueness: true, presence: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
+
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.opne(file_path), filename: 'default-image.jep', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
